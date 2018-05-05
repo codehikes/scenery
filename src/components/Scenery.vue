1f26ec2dd5fe6e21862d7380c5ef0d90faae7383
r0r1 <rori.development@gmail.com>
2018-05-05 15:25:25 +0700

    scenery environment file

---
+++ src/components/Scenery.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ app_name }}</h1>
+    <h2>{{ app_description }}</h2>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/codehike/scenery"
+          target="_blank"
+        >
+          Docs
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      app_name: process.env.APP_NAME,
+      app_description: process.env.APP_DESCRIPTION,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
