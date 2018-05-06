1f6e988c6f74112eaf631c989dd7102965cf0990
r0r1 <rori.development@gmail.com>
2018-05-06 13:36:31 +0700

    add scss loader

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -76,6 +76,16 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
       }
     ]
   },
