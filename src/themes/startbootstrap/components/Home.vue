829ca936debff80f40f7756c49e2fda03578da77
r0r1 <rori.development@gmail.com>
2018-05-12 02:43:07 +0700

    modify home

--- src/themes/startbootstrap/components/Home.vue
+++ src/themes/startbootstrap/components/Home.vue
@@ -3,8 +3,8 @@
   <header class="masthead">
     <div class="container">
       <div class="intro-text">
-        <div class="intro-lead-in">Welcome To Our Studio!</div>
-        <div class="intro-heading text-uppercase">It's Nice To Meet You</div>
+        <div class="intro-lead-in">Welcome To {{ $scenery.name }} !</div>
+        <div class="intro-heading text-uppercase">{{ $scenery.description }}</div>
         <a
           class="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
           href="#services">
